abe227b6ca7daca17b70f94157c7de6d6e972289
hasekoikuta <30440577+hasekoikuta@users.noreply.github.com>
2021-02-27 22:36:01 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var nikki = document.getElementById("nikki");
+var btn = document.getElementById("i-1");
+
+btn.onclick = function () {
+    nikki.classList.toggle("visible");
+    nikki.classList.toggle("hidden");
+}

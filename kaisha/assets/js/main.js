f4f1ff52b20df51a07c148674be616b534e80248
hasekoikuta <30440577+hasekoikuta@users.noreply.github.com>
2021-05-10 09:30:21 +0900

    Add files via upload

--- kaisha/assets/js/main.js
+++ kaisha/assets/js/main.js
@@ -37,8 +37,8 @@ flesh.onclick = function () {
 };
 
 function nextStep(){
-  nFrom += nRange;
-  nTo += nRange;
+  nFrom += nRange - 1;
+  nTo += nRange - 1;
    document.numbers.nFrom.value = nFrom ;
  document.numbers.nTo.value = nTo;
   makeQuizList();
